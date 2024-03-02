0a084175d3ab42008d41dd1e22ad544def85af48
Liber-R <romolo.66@hotmail.it>
2024-03-02 11:53:00 +0100

    First javascript exercise completed

--- Javascript/exercise.js
+++ Javascript/exercise.js
@@ -1,29 +1,14 @@
-function hello() {
-    alert('hello');
-}
-
-function add() {
-    var n = parseInt(document.getElementById('s1').innerHTML);
-    document.getElementById('s1').innerHTML = n+1;
-}
-
-function reset() {
-    document.getElementById('s1').innerHTML = 0;
-}
-
-function over() {
-    console.log('Mouse Over!');
-}
-
 function set() {
     var string = document.getElementById('input1').value;
     document.getElementById('s1').innerHTML = string;
 }
 
-function showDate() {
-    var today = new Date();
-    document.getElementById('date1').value = today;
-    document.getElementById('date2').value = today+1;
+function checkDate() {
+    var date1 = document.getElementById('date1').value;
+    var date2 = document.getElementById('date2').value;
+    if (date1==''||date2=='') {
+        alert('Attention! None of the dates can be empty. Please enter the dates.');
+    }   else if (date1==date2) {
+            alert('Attention! The two dates can\'t be the same. Please change them.');
+    }
 }
-
-document.write("<p>A second paragraph</p>");
