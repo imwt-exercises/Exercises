39a8b76d2db1b3146901b23be7826d70ededa97e
Liber-R <romolo.66@hotmail.it>
2024-03-06 00:55:50 +0100

    phase 3 completed, view adjustment made

--- Javascript/Calculator/clc.js
+++ Javascript/Calculator/clc.js
@@ -7,3 +7,13 @@ function deleter() {
     var n = document.getElementById('s1').innerHTML;
     document.getElementById('s1').innerHTML = 0;
 }
+
+function display(event) {
+    var c = document.getElementById('display').value;
+    var to_add = event.target.value;
+    if (c==0) {
+        document.getElementById('display').value = to_add;
+    } else {
+        document.getElementById('display').value = c + to_add;
+    }
+}
